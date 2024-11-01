c4106f5eb03c3abce8cf2a9cb85a9e286c6928f0
A-200 <124668668+A-200Moving@users.noreply.github.com>
2024-11-01 07:29:03 +0700

    Update Hardcore.lua

--- Hardcore.lua
+++ Hardcore.lua
@@ -1,1 +1,9 @@
+if game.ReplicatedStorage.GameData.LatestRoom.Value ~= 0 then
+  game.Players.LocalPlayer:Kick("Please Execute Script Before Open Door!")
+end
 
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+
+spawn(function()
+    
+end
