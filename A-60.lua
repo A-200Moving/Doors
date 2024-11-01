44036384d1ac7b51b4de4ef2c27b6f3a6d3745c4
A-200 <124668668+A-200Moving@users.noreply.github.com>
2024-11-01 22:45:35 +0700

    Create A-60.lua

---
+++ A-60.lua
@@ -0,0 +1,148 @@
+
+local TS = game:GetService("TweenService")
+
+local can = true
+
+local Spawned = Instance.new("BoolValue")
+Spawned.Name = "A-60"
+Spawned.Value = true
+Spawned.Parent = workspace
+
+local Model = game:GetObjects("rbxassetid://15482662791")[1]
+local entity = Instance.new("Model")
+entity.Name = "A-60"
+entity.Parent = workspace
+Model.Parent = entity
+Model.Name = "Main"
+for _,v in pairs(Model:GetDescendants()) do
+   if v:IsA("Script") then
+      v:Destroy()
+   end
+   if v:IsA("ScreenGui") then
+      v:Destroy()
+   end
+end
+Model.Static.EmitterSize = 5
+Model.Static.DistortionSoundEffect.Level = 0.85
+Model.Static.MaxDistance = 500
+Model.CanCollide = false
+Model.Anchored = true
+local faces = {
+   "rbxassetid://192267375",
+   "rbxassetid://1972219027",
+   "rbxassetid://1822114127",
+   "rbxassetid://3354536350",
+   "rbxassetid://3413871766",
+   "rbxassetid://17865063",
+   
+}
+spawn(function()
+   while wait(Random.new():NextNumber(0.1,0.5)) do
+       if entity and entity.Parent then
+    local face = faces[math.random(1, #faces)]
+       Model.A1.ImageLabel.Image = face
+       local face2 = faces[math.random(1, #faces)]
+       Model.A2.ImageLabel.Image = face2
+      else
+         break
+      end
+   end
+end)
+
+local function Move(target)
+   local dist = (Model.Position - target).Magnitude
+   local tween = TS:Create(Model, TweenInfo.new(dist / 140, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {Position = target})
+   tween:Play()
+   tween.Completed:Wait()
+end
+
+local Rooms = {}
+
+local rooms = workspace.CurrentRooms
+for _,v in pairs(rooms:GetChildren()) do
+   table.insert(Rooms, v)
+end
+
+rooms.ChildAdded:Connect(function(v)
+    table.insert(Rooms, v)
+end)
+
+for _,room in pairs(rooms:GetChildren()) do
+   if room:FindFirstChild("Assets") then
+          if room.Assets:FindFirstChild("Light_Fixtures") then
+              for _,v in pairs(room.Assets:GetDescendants()) do
+                  if v.ClassName == "MeshPart" and v.Material == Enum.Material.Neon then
+                      TS:Create(v, TweenInfo.new(2.5), {Color = Color3.new(1,0,0)}):Play()
+                   end
+                  if v.ClassName == "PointLight" then
+                      TS:Create(v, TweenInfo.new(2.5), {Color = Color3.new(1,0,0)}):Play()
+                 end
+                 if v.ClassName == "SpotLight" then
+                      TS:Create(v, TweenInfo.new(2.5), {Color = Color3.new(1,0,0)}):Play()
+                 end
+              end
+          end
+   end
+end
+
+Model.CFrame = Rooms[1].RoomEntrance.CFrame
+
+wait(2)
+
+spawn(function()
+   local loop
+   loop = game:GetService("RunService").RenderStepped:connect(function()
+      if entity and entity.Parent then
+         local player = game:GetService("Players").LocalPlayer
+      local char = player.Character
+      if char then
+         local hum = char:FindFirstChild("Humanoid")
+         local root = char:FindFirstChild("HumanoidRootPart")
+local origin = Model.Position
+	local charOrigin = root.Position
+
+	if (charOrigin - origin).Magnitude <= 40 then
+		local params = RaycastParams.new()
+		params.FilterType = Enum.RaycastFilterType.Blacklist
+		params.FilterDescendantsInstances = {char, Model}
+
+		local result = workspace:Raycast(origin, charOrigin - origin, params)
+		if not result then
+        if not char:GetAttribute("Hiding") then
+                can = false
+		    hum:TakeDamage(100)
+            ReSt:WaitForChild("GameStats")["Player_".. player.Name].Total.DeathCause.Value = "A-60"
+            loop:Disconnect()
+        end
+		end
+	end
+         end
+      else
+         loop:Disconnect()
+      end
+   end)
+end)
+
+while wait() do
+   for index, room in pairs(Rooms) do
+      if room and room.Parent then
+          Move(room.RoomEntrance.Position)
+      end
+      if room and room.Parent then
+          Move(room.RoomExit.Position)
+      end
+      if index == #Rooms then
+          break
+      end
+   end
+   break
+end
+TS:Create(Model.Static, TweenInfo.new(1.5), {Volume = 0}):Play()
+wait(1.5)
+entity:Destroy()
+
+Spawned:Destroy()
+
+if can then
+
+end
