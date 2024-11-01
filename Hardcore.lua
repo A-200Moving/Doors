53103a5252cd357bb85591dbf10610caf0d3a1bb
A-200 <124668668+A-200Moving@users.noreply.github.com>
2024-11-01 08:18:37 +0700

    Update Hardcore.lua

--- Hardcore.lua
+++ Hardcore.lua
@@ -5,5 +5,8 @@ end
 game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
 
 spawn(function()
-    
+    while wait(math.random(50,100)) do
+      game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/A-200Moving/Doors/refs/heads/main/Matcher.lua"))()
+    end
 end
